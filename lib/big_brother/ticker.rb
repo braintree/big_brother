36dea601cb4bc02eb310566150ab2186318494f5
pg and tony <code@getbraintree.com>
2012-05-14 12:05:48 -0500

    uses synchrony to run the ticker so each call is in a fiber.

--- lib/big_brother/ticker.rb
+++ lib/big_brother/ticker.rb
@@ -1,7 +1,7 @@
 module BigBrother
   class Ticker
     def self.schedule!
-      EventMachine.add_periodic_timer(0.1, &method(:tick))
+      EM::Synchrony.add_periodic_timer(0.1, &method(:tick))
     end
 
     def self.tick
